d399bc87c4727042f88a5441ab1f6bb7342993fe
cayor Hadim Faal <ajifamajobe8@gmail.com>
2021-12-08 17:11:47 +0000

    update

--- src/pages/Addstudent.js
+++ src/pages/Addstudent.js
@@ -1,8 +1,27 @@
 import React,  {Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 class Addstudent extends Component
 {
+    state = {
+        name: '',
+        course: '',
+        email: '',
+        phone: '',
+    }
+
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    saveStudent = async (e) => {
+        e.preventDefault();
+
+        const res = axios.post('/api/add-student', this.state);
+    }
 
     render() {
         return (
@@ -16,11 +35,29 @@ class Addstudent extends Component
             </h4>
             </div>
             <div className="card-body">
-                <form>
-                   <div className="fform-group mb-3">
+                <form onSubmit={this.savveStudent} >
+
+                   <div className="form-group mb-3">
                        <label>Student Name</label>
-                       <input type="text" name="" value="" className="form-control" />
-                         </div> 
+                       <input type="text" name="name" onChange={this.handleInput} value={this.state.name} className="form-control" />
+                   </div> 
+                   <div className="form-group mb-3">
+                       <label>Student Course</label>
+                       <input type="text" name="course" onChange={this.handleInput} value={this.state.course} className="form-control" />
+                   </div>  
+                   <div className="form-group mb-3">
+                       <label>Student Email</label>
+                       <input type="text" name="email" onChange={this.handleInput} value={this.state.email} className="form-control" />
+                   </div>  
+                   <div className="form-group mb-3">
+                       <label>Student Phone</label>
+                       <input type="text" name="phone" onChange={this.handleInput} value={this.state.phone} className="form-control" />
+                   </div>  
+                   
+
+                   <div className="form-group mb-3">
+                       <button type="submit" className="btn btn-primary" >Save Student</button>
+                   </div>
                 </form>
             </div>
             </div>
